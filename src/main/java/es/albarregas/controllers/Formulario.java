5d9eaa8189e681f8bc2ca61c2b0ec9b5e621804b
jmelara02 <jesusmelara77@gmail.com>
2023-09-21 18:37:36 +0200

    Formulario simple
    Formulario simple que pide el nombre, correo y da la opción de seleccionar los gustos del usuario

---
+++ src/main/java/es/albarregas/controllers/Formulario.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package es.albarregas.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jesús
+ */
+@WebServlet(name = "Formulario", urlPatterns = {"/Formulario"})
+public class Formulario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Formulario</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Formulario at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Bienvenido</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Bienvenido at " + request.getContextPath() + "</h1>");
+            out.println("<h2>El método de llamada es " + request.getMethod() + "</h2>");
+            Enumeration<String> campos = request.getParameterNames();
+            String nombre = null;
+            String[] array;
+            out.println("<ul>");
+            while(campos.hasMoreElements()){
+                nombre = campos.nextElement();
+                if(!nombre.startsWith("btn")){
+                out.println("<li>" + nombre + " : " +
+                        request.getParameter(nombre) + "</li>" );
+            }
+                
+                array=request.getParameterValues("Gustos");
+            }
+            out.println("<p><a href=\".\">Menu</a></p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        //processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
